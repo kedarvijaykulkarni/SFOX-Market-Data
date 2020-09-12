190cda0f1c3611139f44bb61af0da72ebb5b4936
Kedar Kulkarni <kedarvijaykulkarni@gmail.com>
2020-09-12 19:35:51 +0530

    add vendor js for websocket service

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import(`vendor/ember-websockets/shims/mock-socket.js`);
+  app.import(`vendor/ember-websockets/mock-socket/mock-socket.js`);
 
   return app.toTree();
 };
