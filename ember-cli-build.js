f0960fb3280668845b7d2b1e750ae381b1689e21
Kedar Kulkarni <kedarvijaykulkarni@gmail.com>
2020-09-13 18:33:28 +0530

    added tachyons css into the project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        `node_modules/tachyons-sass`
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
